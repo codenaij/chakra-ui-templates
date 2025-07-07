27ac37df15062581157372032b3b3c49b4704ffd
chinonso-opti <work>
2025-07-08 00:06:29 +0100

    added biome and refactor

--- src/theme/recipies/radioRecipe.ts
+++ src/theme/recipies/radioRecipe.ts
@@ -1,60 +1,6 @@
 import { defineSlotRecipe } from "@chakra-ui/react";
 import { radioGroupAnatomy } from "@chakra-ui/react/anatomy";
 
-const variants = {
-	roundedLightOutlineBrand: {
-		root: {
-			borderWidth: "1px",
-			borderStyle: "solid",
-			borderColor: "black",
-			padding: "2px",
-			borderRadius: "6px",
-		},
-		item: {
-			justifyContent: "center",
-			borderWidth: "1px",
-			borderStyle: "solid",
-			borderColor: "transparent",
-			padding: "6px 10px",
-			width: "50%",
-			color: "black",
-			_checked: {
-				width: "50%",
-				borderRadius: "6px",
-				background: "teal",
-				borderColor: "black",
-				borderWidth: "1px",
-				zIndex: "100",
-				_hover: {
-					backgroundColor: "teal",
-				},
-				_disabled: {
-					background: "gray.130",
-					borderColor: "gray.350",
-				},
-			},
-			_hover: {
-				cursor: "pointer",
-				backgroundColor: "teal",
-				borderRadius: "6px",
-			},
-		},
-		itemControl: {
-			display: "none",
-		},
-		itemText: {
-			color: "gray.450",
-			fontSize: "sm",
-			_checked: {
-				color: "brand.600",
-			},
-			_hover: {
-				color: "brand.600",
-			},
-		},
-	},
-};
-
 export const radioGroupSlotRecipe = defineSlotRecipe({
 	slots: radioGroupAnatomy.keys(),
 	base: {
@@ -67,6 +13,58 @@ export const radioGroupSlotRecipe = defineSlotRecipe({
 		},
 	},
 	variants: {
-		variant: variants,
+		variant: {
+			roundedLightOutlineBrand: {
+				root: {
+					borderWidth: "1px",
+					borderStyle: "solid",
+					borderColor: "black",
+					padding: "2px",
+					borderRadius: "6px",
+				},
+				item: {
+					justifyContent: "center",
+					borderWidth: "1px",
+					borderStyle: "solid",
+					borderColor: "transparent",
+					padding: "6px 10px",
+					width: "50%",
+					color: "black",
+					_checked: {
+						width: "50%",
+						borderRadius: "6px",
+						background: "teal",
+						borderColor: "black",
+						borderWidth: "1px",
+						zIndex: "100",
+						_hover: {
+							backgroundColor: "teal",
+						},
+						_disabled: {
+							background: "gray.130",
+							borderColor: "gray.350",
+						},
+					},
+					_hover: {
+						cursor: "pointer",
+						backgroundColor: "teal",
+						borderRadius: "6px",
+					},
+				},
+				itemControl: {
+					display: "none",
+				},
+				itemText: {
+					color: "gray.450",
+					fontSize: "sm",
+					_checked: {
+						color: "brand.600",
+					},
+					_hover: {
+						color: "brand.600",
+					},
+				},
+			},
+		},
 	},
 });
